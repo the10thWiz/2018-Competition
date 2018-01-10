4ff19cab8e0726d015548b0947a4aad399f1acc6
jay <jay@jay-Lenovo-ideapad-510S-14ISK>
2018-01-10 00:23:35 -0600

    Reworked plate position polling

--- 2018 Competition Bot/src/org/usfirst/frc/team1732/robot/autotools/DriverStationData.java
+++ 2018 Competition Bot/src/org/usfirst/frc/team1732/robot/autotools/DriverStationData.java
@@ -5,25 +5,43 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class DriverStationData {
 
+	public static TeeterTotter closeSwitch;
+	public static TeeterTotter scale;
+	public static TeeterTotter farSwitch;
+
 	private static String platePosition = "";
 
 	public static void pollPlatePosition() {
 		platePosition = DriverStation.getInstance().getGameSpecificMessage();
+		if (platePosition.length() >= 3) {
+			Alliance a = getAlliance();
+			closeSwitch = new TeeterTotter(a, platePosition.charAt(0));
+			scale = new TeeterTotter(a, platePosition.charAt(1));
+			farSwitch = new TeeterTotter(a, platePosition.charAt(2));
+		}
 	}
 
-	public static boolean isCloseSwitchPlateLeft() {
-		return platePosition.charAt(0) == 'L';
+	public static Alliance getAlliance() {
+		return DriverStation.getInstance().getAlliance();
 	}
 
-	public static boolean isScalePlateLeft() {
-		return platePosition.charAt(0) == 'L';
-	}
+	public static class TeeterTotter {
 
-	public static boolean isFarSwitchPlateLeft() {
-		return platePosition.charAt(0) == 'L';
-	}
+		public final Alliance leftPlateAlliance;
+		public final Alliance rightPlateAlliance;
 
-	public static boolean isRedAlliance() {
-		return DriverStation.getInstance().getAlliance().equals(Alliance.Red);
+		public TeeterTotter(Alliance ourAlliance, char side) {
+			if (side == 'L') {
+				leftPlateAlliance = ourAlliance;
+				rightPlateAlliance = getOpposite(ourAlliance);
+			} else {
+				leftPlateAlliance = getOpposite(ourAlliance);
+				rightPlateAlliance = ourAlliance;
+			}
+		}
+
+		private static Alliance getOpposite(Alliance a) {
+			return a.equals(Alliance.Red) ? Alliance.Blue : Alliance.Red;
+		}
 	}
 }
