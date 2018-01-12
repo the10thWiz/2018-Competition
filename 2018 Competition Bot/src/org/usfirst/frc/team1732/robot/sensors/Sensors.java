c306eea71d361acbe4d059d538f4344193950dd6
Jay Schauer <lacrossejason@gmail.com>
2018-01-12 11:58:20 -0600

    Fixed navXusb name as a test commit

--- 2018 Competition Bot/src/org/usfirst/frc/team1732/robot/sensors/Sensors.java
+++ 2018 Competition Bot/src/org/usfirst/frc/team1732/robot/sensors/Sensors.java
@@ -13,7 +13,7 @@ public class Sensors {
 
 	public final AHRS navX = new AHRS(SPI.Port.kMXP, (byte) 100);
 	// if we use USB we can only get processed data, but that's fine
-	public final AHRS nacXusb = new AHRS(SerialPort.Port.kUSB, SerialDataType.kProcessedData, (byte) 100);
+	public final AHRS navXusb = new AHRS(SerialPort.Port.kUSB, SerialDataType.kProcessedData, (byte) 100);
 
 	public final TalonEncoder leftEncoder = Robot.drivetrain.leftEncoder;
 	public final TalonEncoder rightEncoder = Robot.drivetrain.rightEncoder;
